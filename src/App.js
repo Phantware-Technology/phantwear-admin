b7f232f7f2bd65f19c036b1d6a1fa85d82336510
phantware <phantmoney2011@gmail.com>
2023-04-07 21:04:54 +0100

    UserList folder and files added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import Sidebar from './components/sidebar/Sidebar'
 import Topbar from './components/topbar/Topbar'
 import './App.css'
 import Home from './pages/home/Home'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import UserList from './pages/userList/UserList'
 import User from './pages/user/User'
 import NewUser from './pages/newUser/NewUser'
@@ -20,42 +15,26 @@ import { useSelector } from 'react-redux'
 function App() {
   const admin = useSelector((state) => state.user.currentUser.isAdmin)
   return (
-    <Router>
-      <Switch>
-        <Route path='/login'>
-          <Login />
-        </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
         {admin && (
           <>
             <Topbar />
             <div className='container'>
               <Sidebar />
-              <Route exact path='/'>
-                <Home />
-              </Route>
-              <Route path='/users'>
-                <UserList />
-              </Route>
-              <Route path='/user/:userId'>
-                <User />
-              </Route>
-              <Route path='/newUser'>
-                <NewUser />
-              </Route>
-              <Route path='/products'>
-                <ProductList />
-              </Route>
-              <Route path='/product/:productId'>
-                <Product />
-              </Route>
-              <Route path='/newproduct'>
-                <NewProduct />
-              </Route>
+              <Route path='/' element={<Home />} />
+              <Route path='/users' element={<UserList />} />
+              <Route path='/user/:userId' element={<User />} />
+              <Route path='/newUser' element={<NewUser />} />
+              <Route path='/products' element={<ProductList />} />
+              <Route path='/product/:productId' element={<Product />} />
+              <Route path='/newproduct' element={<NewProduct />} />
             </div>
           </>
         )}
-      </Switch>
-    </Router>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
