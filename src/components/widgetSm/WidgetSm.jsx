dc7cf3256a5e667221b1f0702ca9dc9455446ed8
phantware <phantmoney2011@gmail.com>
2023-04-09 07:50:00 +0100

    React imported in all pages

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,45 +1,46 @@
-import "./widgetSm.css";
-import { Visibility } from "@material-ui/icons";
-import { useEffect, useState } from "react";
-import { userRequest } from "../../requestMethods";
+import React from 'react'
+import './widgetSm.css'
+import { Visibility } from '@material-ui/icons'
+import { useEffect, useState } from 'react'
+import { userRequest } from '../../requestMethods'
 
 export default function WidgetSm() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const res = await userRequest.get("users/?new=true");
-        setUsers(res.data);
+        const res = await userRequest.get('users/?new=true')
+        setUsers(res.data)
       } catch {}
-    };
-    getUsers();
-  }, []);
-  
+    }
+    getUsers()
+  }, [])
+
   return (
-    <div className="widgetSm">
-      <span className="widgetSmTitle">New Join Members</span>
-      <ul className="widgetSmList">
+    <div className='widgetSm'>
+      <span className='widgetSmTitle'>New Join Members</span>
+      <ul className='widgetSmList'>
         {users.map((user) => (
-          <li className="widgetSmListItem" key={user._id}>
+          <li className='widgetSmListItem' key={user._id}>
             <img
               src={
                 user.img ||
-                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                'https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif'
               }
-              alt=""
-              className="widgetSmImg"
+              alt=''
+              className='widgetSmImg'
             />
-            <div className="widgetSmUser">
-              <span className="widgetSmUsername">{user.username}</span>
+            <div className='widgetSmUser'>
+              <span className='widgetSmUsername'>{user.username}</span>
             </div>
-            <button className="widgetSmButton">
-              <Visibility className="widgetSmIcon" />
+            <button className='widgetSmButton'>
+              <Visibility className='widgetSmIcon' />
               Display
             </button>
           </li>
         ))}
       </ul>
     </div>
-  );
+  )
 }
