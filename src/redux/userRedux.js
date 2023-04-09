7183aff229cc8e62de0651ab8ad65d914ac2538b
phantware <phantmoney2011@gmail.com>
2023-04-09 11:15:51 +0100

    utils folder and file added

--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -1,7 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit'
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
     currentUser: null,
     isFetching: false,
@@ -9,21 +8,18 @@ const userSlice = createSlice({
   },
   reducers: {
     loginStart: (state) => {
-      state.isFetching = true;
+      state.isFetching = true
     },
     loginSuccess: (state, action) => {
-      state.isFetching = false;
-      state.currentUser = action.payload;
+      state.isFetching = false
+      state.currentUser = action.payload
     },
     loginFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
-    },
-    logout: (state) => {
-      state.currentUser = null;
+      state.isFetching = false
+      state.error = true
     },
   },
-});
+})
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
-export default userSlice.reducer;
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions
+export default userSlice.reducer
---
+++ src/utils/requestMethods.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const dotenv = require('dotenv')
+dotenv.config()
+
+const BASE_URL = 'http://localhost:5000/api/'
+// const BASE_URL = 'https://e-commerce-api-z6rv.onrender.com/api/'
+
+const TOKEN = process.env.TOKEN
+// export const publicRequest = axios.create({
+//   baseURL: BASE_URL,
+// })
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  ...(TOKEN && { header: { token: `Bearer ${TOKEN}` } }),
+})
+
+axiosInstance.interceptors.request.use(
+  function async(config) {
+    config.headers.contentType = 'application/json'
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+export default axiosInstance
