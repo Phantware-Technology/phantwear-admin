a99cf0c12097e8e9f17f71af557957e1df105bff
phantware <phantmoney2011@gmail.com>
2023-04-23 13:25:18 +0100

    productRedux file modified

--- src/redux/productRedux.js
+++ src/redux/productRedux.js
@@ -1,7 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 export const productSlice = createSlice({
-  name: "product",
+  name: 'product',
   initialState: {
     products: [],
     isFetching: false,
@@ -10,63 +10,63 @@ export const productSlice = createSlice({
   reducers: {
     //GET ALL
     getProductStart: (state) => {
-      state.isFetching = true;
-      state.error = false;
+      state.isFetching = true
+      state.error = false
     },
     getProductSuccess: (state, action) => {
-      state.isFetching = false;
-      state.products = action.payload;
+      state.isFetching = false
+      state.products = action.payload
     },
     getProductFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
+      state.isFetching = false
+      state.error = true
     },
     //DELETE
     deleteProductStart: (state) => {
-      state.isFetching = true;
-      state.error = false;
+      state.isFetching = true
+      state.error = false
     },
     deleteProductSuccess: (state, action) => {
-      state.isFetching = false;
+      state.isFetching = false
       state.products.splice(
         state.products.findIndex((item) => item._id === action.payload),
         1
-      );
+      )
     },
     deleteProductFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
+      state.isFetching = false
+      state.error = true
     },
     //UPDATE
     updateProductStart: (state) => {
-      state.isFetching = true;
-      state.error = false;
+      state.isFetching = true
+      state.error = false
     },
     updateProductSuccess: (state, action) => {
-      state.isFetching = false;
+      state.isFetching = false
       state.products[
         state.products.findIndex((item) => item._id === action.payload.id)
-      ] = action.payload.product;
+      ] = action.payload.product
     },
     updateProductFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
+      state.isFetching = false
+      state.error = true
     },
     //UPDATE
     addProductStart: (state) => {
-      state.isFetching = true;
-      state.error = false;
+      state.isFetching = true
+      state.error = false
     },
     addProductSuccess: (state, action) => {
-      state.isFetching = false;
-      state.products.push(action.payload);
+      state.isFetching = false
+      state.products.push(action.payload)
     },
     addProductFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
+      state.isFetching = false
+      state.error = true
     },
   },
-});
+})
 
 export const {
   getProductStart,
@@ -81,6 +81,6 @@ export const {
   addProductStart,
   addProductSuccess,
   addProductFailure,
-} = productSlice.actions;
+} = productSlice.actions
 
-export default productSlice.reducer;
+export default productSlice.reducer
