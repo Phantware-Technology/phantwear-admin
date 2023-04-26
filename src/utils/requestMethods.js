0be4a9087a6ca81ef7a231892e6dd9e209fc431c
phantware <phantmoney2011@gmail.com>
2023-04-26 06:34:52 +0100

    requestMethods.js file modified

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 const dotenv = require('dotenv')
 dotenv.config()
 
-const BASE_URL = 'http://localhost:5000/api/'
-// const BASE_URL = 'https://e-commerce-api-z6rv.onrender.com/api/'
+// const BASE_URL = 'http://localhost:5000/api/'
+const BASE_URL = 'https://e-commerce-api-z6rv.onrender.com/api/'
 
 // const TOKEN = process.env.TOKEN
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
